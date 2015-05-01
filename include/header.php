4f5c8506af130494e506f934b3f1310cf7237b7f
kiran <askiran123@gmail.com>
2015-05-01 23:34:27 +0530

    Header added

--- include/header.php
+++ include/header.php
@@ -2,19 +2,18 @@
         <div class="navbar navbar-default top--header" role="navigation">
             <div class="container">
                 <div class="row">                    
-                        <a href="#" class="col-sm-2 logo"><span></span><img src="img/rv_institutes.png" class="img-responsive" /></a> 
-                        <a href="#" class="col-sm-2 logo"><span></span><img src="img/rv_vlsi.png" class="img-responsive" /></a> 
-                        <a href="#" class="col-sm-2 logo"><span></span><img src="img/nanochip.png" class="img-responsive" /></a>  
+                        <a href="index.php" class="col-sm-2 logo"><span></span><img src="img/rv_institutes.png" class="img-responsive" /></a> 
+                        <a href="index.php" class="col-sm-2 logo"><span></span><img src="img/rv_vlsi.png" class="img-responsive" /></a> 
+                        <a href="index.php" class="col-sm-2 logo"><span></span><img src="img/nanochip.png" class="img-responsive" /></a>  
                         <div class="col-sm-7 col-sm-offset-5 col-md-6 col-md-offset-6 mar-t25 mar-b30">
                         <div class="navbar-right pad-t10">
                            <div class="clearfix">
                                 <ul class="pull-left h-small-nav pad-t5">
-                                    <li><a href="#">Gallery</a></li>
-                                    <li><a href="#">FAQ</a></li>
-                                    <li><a href="#">Contact Us</a></li>
+                                    <li><a href="faq.php">FAQ</a></li>
+                                    <li><a href="contact_us.php">Contact Us</a></li>
                                 </ul>
                                 <ul class="pull-left h-social">
-                                    <li><a href="#">Facebook</a></li>
+                                    <li><a href="https://www.facebook.com/rv.vlsi" target='_blank'>Facebook</a></li>
                                     <li><a href="#">Twitter</a></li>
                                     <li><a href="#">Linked In</a></li>
                                     <li><a href="#">Youtube</a></li>
@@ -45,6 +44,10 @@
                                 <li><a href="placements.php" class="pad-sm-t13 pad-sm-b12">Placements</a>
                                 </li>
                                 <li><a href="testimonial.php" class="pad-sm-t13 pad-sm-b12 pad-r0">Testimonials</a>
+                                </li>
+                                <li><a href="faq.php" class="pad-sm-t13 pad-sm-b12 pad-r0">FAQ</a>
+                                </li>
+                                <li><a href="contact_us.php" class="pad-sm-t13 pad-sm-b12 pad-r0">Contact Us</a>
                                 </li>
                                 <li style='display:none'><a href="#" class="pad-sm-t13 pad-sm-b12 pad-r0">B.E Projects</a>
                                 </li>                                
